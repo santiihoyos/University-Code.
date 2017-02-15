84a1b574ce1abc6f4f33f2c839ca76e022f0ec46
SantiiHoyos <santiago.hoyos93@gmail.com>
2017-02-15 20:07:35 +0100

    mejorado ejercicio propuesto 1 y añadido el 2 TEMA 2

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,23 +1,24 @@
 /*
- * @File:   Ejercicio1.c - Ejercicio i
+ * @File:   Ejercicio2.c - Ejercicio j
  *
- * Sinopsis: Primer ejercicio propuesto del tema 2.
+ * Sinopsis: Segundo ejercicio propuesto del tema 2.
  *
  * El progrma realiza una petición al usuario para que introduzca una cantidad de
  * segundos, posteirormente se le muestra esa cantidad y su equivalente en
- * minutos y lo que sobra en segundos.
+ * horas, minutos y lo que sobra en segundos.
  *
  * @author: Santiago Hoyos Zea
  *
- * fecha: 13/02/2017 19:02
+ * fecha: 15/02/2017 19:36
  *
- * @version 0.2
+ * @version 0.1
  */
 
 #include <stdio.h>
 
 #define minutosLiteral "minutos"
 #define segundosLiteral "segundos"
+#define horasLiteral "horas"
 
 /**
  * Funcion principal.
@@ -26,19 +27,19 @@
 int main() {
 
     //Declaramos un long para que no nos quedemos cortos.
-    long segundos, minutos;
+    long segundos, minutos, horas;
 
     //Recogida de datos
     printf("Escribe una cantidad de segundos: ");
     scanf("%ld", &segundos);
 
     //Operaciones
-    minutos = segundos / 60;
-    segundos = segundos % 60;
+    horas = segundos / 3600;
+    minutos = (segundos % 3600) / 60;
+    segundos = (segundos % 3600) % 60;
 
-    printf("%ld %s ... %ld %s.", minutos, minutosLiteral, segundos, segundosLiteral);
+    printf("%ld %s ... %ld %s ... %ld %s.", horas, horasLiteral, minutos, minutosLiteral, segundos, segundosLiteral);
 
     //Acabamos el programa sin errres
     return 0;
 }
-
